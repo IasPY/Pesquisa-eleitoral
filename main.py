6b748a4a4903e4c21d0895b3ae12bb5df3105c6e
IasDEV <121503597+IasPY@users.noreply.github.com>
2024-08-23 20:58:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,85 @@
+print('Baseado na eleição 2022')
+puxa_numero = input('Digite um número eleitoral: ')
+nome = ''
+vice = ''
+partido = ''
+ocupações = ''
+print('-------------------------------')
+if puxa_numero == '13':
+  nome = ''.join(
+      'Candidato à presidencia do Brasil: Luiz Inácio Lula da Silva')
+  vice = ''.join('Vice-Presidente: Geraldo José Rodrigues Alckmin Filho')
+  partido = ''.join('Partido: PT (Partido dos Trabalhadores)')
+  ocupacoes = ''.join(
+      'Ocupações anteriores: Ex-Metalurgico, Ex-sindicalista, Ex-Político, Ex-presidiário'
+  )
+
+  print(f'{nome} \n{vice} \n{partido} \n{ocupacoes}')
+if puxa_numero == '22':
+  nome = ''.join('Candidato à presidencia do Brasil: Jair Messias Bolsonaro')
+  vice = ''.join('Vice: Braga Netto')
+  partido = ''.join('Partido: PL (Partido Liberal)')
+  ocupacoes = ''.join('ocupações anteriores:Ex-militar, Atual Presidente')
+
+  print(f'{nome}, \n{vice}, \n{partido}, \n{ocupacoes}')
+
+if puxa_numero == '15':
+  nome = ''.join('Candidato: Simone Nassar Tebet')
+  vice = ''.join('Vice: MARA GABRILLI ')
+  partido = ''.join('Partido: MDB (Movimendo Democrático Brasileiro)')
+  ocupacoes = ''.join(
+      'ocupações: Advogada, Professora, Política, ex-funcionaria pública')
+
+  print(f'{nome}, \n{vice}, \n{partido}, \n{ocupacoes}')
+if puxa_numero == '12':
+  nome = ''.join('Candidato: Ciro Ferreira Gomes')
+  vice = ''.join('Vice: ANA PAULA MATOS')
+  partido = ''.join('Partido: PDT (Partido Democrático Trabalhista)')
+  ocupacoes = ''.join('ocupações: Advogado, político, Professor Universitário')
+
+  print(f'{nome}, \n{vice}, \n{partido}, \n{ocupacoes}')
+if puxa_numero == '44':
+  nome = ''.join('Candidato: Soraya Vieira Thronicke')
+  vice = ''.join('Vice: MARCOS CINTRA')
+  partido = ''.join('Partido: União Brasil')
+  ocupacoes = ''.join('ocupações: Poítica, advogada')
+
+  print(f'{nome}, \n{vice}, \n{partido}, \n{ocupacoes}')
+if puxa_numero == '30':
+  nome = ''.join('Candidato: Luiz Felipe Chaves D’Avila')
+  vice = ''.join('Vice: TIAGO MITRAUD')
+  partido = ''.join('Partido: NOVO')
+  ocupacoes = ''.join('ocupações: Advogado, ')
+
+  print(f'{nome}, \n{vice}, \n{partido}, \n{ocupacoes}')
+if puxa_numero == '14':
+  nome = ''.join('Candidato: Kelmon Luís da Silva Souza')
+  vice = ''.join('Vice: PASTOR GAMONAL')
+  partido = ''.join('Partido: PTB (Partido Trabalhista Brasileiro)')
+  ocupacoes = ''.join('ocupações: Pastor Ortodoxo, poliítico')
+
+  print(f'{nome}, \n{vice}, \n{partido}, \n{ocupacoes}')
+if puxa_numero == '80':
+  nome = ''.join('Candidato: LÉO PÉRICLES')
+  vice = ''.join('Vice: SAMARA MARTINS')
+  partido = ''.join('Partido: UP (Unidade Popular)')
+  ocupacoes = ''.join('ocupações: Político e Ativista Social')
+
+  print(f'{nome}, \n{vice}, \n{partido}, \n{ocupacoes}')
+if puxa_numero == '21':
+  nome = ''.join('Candidato: Sofia Padua Manzano')
+  vice = ''.join('Vice: ANTONIO ALVES')
+  partido = ''.join('Partido: PCB (PArtido Comunista Brasileiro)')
+  ocupacoes = ''.join(
+      'ocupações: economista, professora, e militante comunista')
+
+  print(f'{nome}, \n{vice}, \n{partido}, \n{ocupacoes}')
+if puxa_numero == '':
+  nome = ''.join('Candidato: Vera Lúcia Pereira da Silva Salgado')
+  vice = ''.join('Vice: RAQUEL TREMEMBÉ')
+  partido = ''.join(
+      'Partido: PSTU (Partido Socialista dos Trabalhadores Unificado)')
+  ocupacoes = ''.join('ocupações: Política, militante ')
+
+  print(f'{nome}, \n{vice}, \n{partido}, \n{ocupacoes}')\
+#feito por isaias 2B
